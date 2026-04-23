13a363db8676f3d7b0542dc948efd360da20197d
TranNguyenPhuc <trannguyenphuc1902@gmail.com>
2026-04-23 15:58:06 +0700

    feat: update component states during runtime

--- src/main/java/com/javasim/model/CircuitGraph.java
+++ src/main/java/com/javasim/model/CircuitGraph.java
@@ -41,15 +41,19 @@ public class CircuitGraph {
         // 3. Sync voltages back to components
         // The first 'nodeCount' elements of the solution are the node voltages
         for (Component comp : components) {
+            int[] nodes = comp.GetNodeIds();
+            if (nodes.length < 2) continue;
+
+            // Extract voltages from the solution (Node 0 is Ground)
+            double vA = (nodes[0] > 0) ? solution[nodes[0] - 1] : 0.0;
+            double vB = (nodes[1] > 0) ? solution[nodes[1] - 1] : 0.0;
+
             if (comp instanceof Capacitor) {
-                // Sync the specific node voltages for this component
-                int n1 = comp.GetNodeIds()[0];
-                int n2 = comp.GetNodeIds()[1];
-                
-                double v1 = (n1 > 0) ? solution[n1 - 1] : 0.0;
-                double v2 = (n2 > 0) ? solution[n2 - 1] : 0.0;
-                
-                ((Capacitor) comp).RecordPhysicsState(v1, v2);
+                ((Capacitor) comp).RecordPhysicsState(vA, vB); //
+            } else if (comp instanceof Inductor) {
+                ((Inductor) comp).UpdateCurrent(vA, vB, deltaTime); //
+            } else if (comp instanceof Bulb) {
+                ((Bulb) comp).CheckStatus(vA, vB); //
             }
         }
     }
